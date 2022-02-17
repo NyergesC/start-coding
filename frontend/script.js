e7be104b864b18e773b7bc4195a5ddbddecd7bc4
NyergesC <cintia.nyerges@gmail.com>
2022-02-17 14:46:40 +0100

    zgag

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,11 @@
-function getDateOfHighestDeposit (account) {
+function getDateOfHighestDeposit(account) {
     const dateOfHighest = NULL;
     const amountOfHighest = 0;
 
     for (const transactions of account) {
 
         if (typeof transactions === "deposit" && transactions === isSuccessful) {
-            console.log()
+            
         }
         
     }

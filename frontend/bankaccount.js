fdf61410142f7c76c17c487baaa7fdf9185d8b60
NyergesC <cintia.nyerges@gmail.com>
2022-02-17 12:27:19 +0100

    justbegin

---
+++ frontend/bankaccount.js
@@ -0,0 +1,89 @@
+let account1 = {
+    "id": 123456781234567812345678,
+    "name": "Kiss Lilla",
+    "balance": 3.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 200,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 100,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 500,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+};
+
+let account2 = {
+    "id": 234567892345678923456789,
+    "name": "Kiss Gergely",
+    "balance": 5.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 400,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 500,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 600,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+};
+
+let account3 = {
+    "id": 345678913456789134567891,
+    "name": "Nagy Lajos",
+    "balance": 8.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 100,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 900,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 800,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+},
---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+function getDateOfHighestDeposit (account) {
+    const dateOfHighest = NULL;
+    const amountOfHighest = 0;
+
+    for (const transactions of account) {
+
+        if (typeof transactions === "deposit" && transactions === isSuccessful) {
+            console.log()
+        }
+        
+    }
+
+
+}

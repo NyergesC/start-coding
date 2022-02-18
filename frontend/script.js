db62bf37d972e946ff1bbf79c4fc6a8fac4ccb17
NyergesC <cintia.nyerges@gmail.com>
2022-02-18 19:32:03 +0100

    task1

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,117 @@
-function getDateOfHighestDeposit (account) {
-    const dateOfHighest = NULL;
-    const amountOfHighest = 0;
+let account1 = {
+    "id": 123456781234567812345678,
+    "name": "Kiss Lilla",
+    "balance": 3.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 200,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 100,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 500,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+};
 
-    for (const transactions of account) {
+let account2 = {
+    "id": 234567892345678923456789,
+    "name": "Kiss Gergely",
+    "balance": 5.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 400,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 500,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 600,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+};
 
-        if (typeof transactions === "deposit" && transactions === isSuccessful) {
-            console.log()
+let account3 = {
+    "id": 345678913456789134567891,
+    "name": "Nagy Lajos",
+    "balance": 8.000,
+    "currency": "USA",
+    "transactions": [
+            {
+                "amount": 100,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2019
+            },
+            {
+                "amount": 900,
+                "currency": "USA",
+                "isSuccessful": true,
+                "type": "withdraw",
+                "date": 2020
+            },
+            {
+                "amount": 800,
+                "currency": "USA",
+                "isSuccessful": false,
+                "type": "deposit",
+                "date": 2021
+            }
+    ]
+};
+
+function getDateOfHighestDeposit(account) {
+    let dateOfHighest = null;
+    let amountOfHighest = 0;
+
+    for (let transactions of account.transactions) {
+
+        let isRelevant = false
+
+        if (transactions.type === "deposit" && transactions.isSuccessful === true) {
+            isRelevant = true
+        }else {
+            isRelevant = false
+        };
+
+        if (isRelevant === true && transactions.amount > amountOfHighest) {
+            dateOfHighest = transactions.date;
+            amountOfHighest = transactions.amount;
         }
+
+     return dateOfHighest;   
         
     }
-
-
 }
+
+console.log(getDateOfHighestDeposit(account1));
+console.log(getDateOfHighestDeposit(account2));
+console.log(getDateOfHighestDeposit(account3));

adce524dd9e601a9b333f9db60aa96defc57802a
NyergesC <cintia.nyerges@gmail.com>
2022-02-19 16:22:43 +0100

    task 2

---
+++ frontend/task2.js
@@ -0,0 +1,35 @@
+
+let doors = [];
+
+for (let i = 1; i <= 100; i++) {
+    doors.push({
+        "isOpen": false,
+        "doorNumber": i
+    })
+    
+};
+
+for (let i = 1; i <= 100; i++) {
+
+    for (let door of doors) {
+
+        let shouldToggle = door.doorNumber % i
+        
+        if (shouldToggle === 0) {
+            door.isOpen = !door.isOpen;
+
+        };
+    };
+
+};
+
+for (let door of doors) {
+    if (door.isOpen === true) {
+        console.log(door.doorNumber);
+    }
+
+}
+
+
+
+    
